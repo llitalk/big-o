690e178ffd232ea29f24adb559ee7ee5e7093564
llitalk <litalkaminsky@gmail.com>
2024-05-13 10:11:32 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css';
+//0(N) - LINEAR TIME
+function add(n) {
+  var sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum = +1;
+  }
+  //if n will be 100 it will take 100 times
+}
+
+//0(1) constant time
